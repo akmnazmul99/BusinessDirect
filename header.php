f295b653136300c0a9c53f39917da6e68e472a9c
A. K. M. NAZMUL ISLAM <nazmulislamredoy@gmail.com>
2017-10-22 14:18:17 +0600

    bug fixing

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
         <!--[if lt IE 8]>
            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
        <![endif]-->
-        <title>Business Direct</title>
+        <title>UK Energy Business</title>
 
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
