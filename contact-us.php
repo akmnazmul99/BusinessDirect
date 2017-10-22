9bafd92faf9882f74411d8ab8498991e132e8393
A. K. M. NAZMUL ISLAM <nazmulislamredoy@gmail.com>
2017-10-22 14:06:01 +0600

    bug fixing

--- contact-us.php
+++ contact-us.php
@@ -48,134 +48,138 @@
 <div class="wrapper">
     <div class="container contact-us">
         <div class="row form-group">
-            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 form wow fadeInLeftBig" data-wow-duration="1s">
-                <form>
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 form-group">
+                <div class="form wow fadeInLeftBig" data-wow-duration="1s">
+                    <form>
+                        <div class="row form-group">
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                <h3>Please take a moment to fill in the form</h3>
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                                <label class="control_label">Name</label>
+                            </div>
+                            <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
+                                <div class="input-group">
+                                    <div class="input-group-addon"><i class="fa fa-user"></i></div>
+                                    <input type="text" class="form-control" id="username" placeholder="Enter Name">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                                <label class="control_label">Email <span class="color-red">*</span></label>
+                            </div>
+                            <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
+                                <div class="input-group">
+                                    <div class="input-group-addon"><i class="fa fa-envelope"></i></div>
+                                    <input type="email" class="form-control" id="username" placeholder="Enter Email">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                                <label class="control_label">Company Name <span class="color-red">*</span></label>
+                            </div>
+                            <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
+                                <div class="input-group">
+                                    <div class="input-group-addon"><i class="fa fa-home"></i></div>
+                                    <input type="text" class="form-control" id="username" placeholder="Enter Company Name">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                                <label class="control_label">Phone Number <span class="color-red">*</span></label>
+                            </div>
+                            <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
+                                <div class="input-group">
+                                    <div class="input-group-addon"><i class="fa fa-phone"></i></div>
+                                    <input type="text" class="form-control" id="username" placeholder="Enter Phone Number">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-xs-12 col-sm-offset-3 col-sm-9 col-md-offset-3 col-md-9 col-lg-offset-3 col-lg-9">
+                                <div class="check-box">
+                                    <label>
+                                        <input type="checkbox"> <span>I am interested in a price comparison for my business energy</span>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-xs-12 col-sm-offset-3 col-sm-9 col-md-offset-3 col-md-9 col-lg-offset-3 col-lg-9">
+                                <div class="check-box">
+                                    <label>
+                                        <input type="checkbox"> <span>I want to reduce my business energy consumption</span>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-xs-12 col-sm-offset-3 col-sm-9 col-md-offset-3 col-md-9 col-lg-offset-3 col-lg-9">
+                                <div class="check-box">
+                                    <label>
+                                        <input type="checkbox"> <span>I am interested to join UK Energy Business </span>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-xs-12 col-sm-offset-3 col-sm-9 col-md-offset-3 col-md-9 col-lg-offset-3 col-lg-9">
+                                <div class="check-box">
+                                    <label>
+                                        <input type="checkbox"> <span>I need help to understand my bills</span>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-xs-12 col-sm-offset-3 col-sm-9 col-md-offset-3 col-md-9 col-lg-offset-3 col-lg-9">
+                                <textarea class="form-control" rows="5" placeholder="How we can Help?"></textarea>
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-xs-12 col-sm-offset-3 col-sm-9 col-md-offset-3 col-md-9 col-lg-offset-3 col-lg-9">
+                                <a class="anchor-holder custom-button margin-top-20px">Submit</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-offset-1 col-md-5 col-lg-offset-1 col-lg-5">
+                <div class="our-office wow fadeInRightBig" data-wow-duration="1s">
                     <div class="row form-group">
                         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                            <h3>Please take a moment to fill in the form</h3>
+                            <h3>Our Office</h3>
                         </div>
                     </div>
                     <div class="row form-group">
-                        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
-                            <label class="control_label">Name</label>
-                        </div>
-                        <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
-                            <div class="input-group">
-                                <div class="input-group-addon"><i class="fa fa-user"></i></div>
-                                <input type="text" class="form-control" id="username" placeholder="Enter Name">
-                            </div>
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            <i class="fa fa-home"></i>  <span>UK ENERGY BUSINESS </span>
                         </div>
                     </div>
                     <div class="row form-group">
-                        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
-                            <label class="control_label">Email <span class="color-red">*</span></label>
-                        </div>
-                        <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
-                            <div class="input-group">
-                                <div class="input-group-addon"><i class="fa fa-envelope"></i></div>
-                                <input type="email" class="form-control" id="username" placeholder="Enter Email">
-                            </div>
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            <i class="fa fa-address-book"></i>  <span>98-102, Cranbrook Road(1st FL) Ilford Essex IG1 4NH</span>
                         </div>
                     </div>
                     <div class="row form-group">
-                        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
-                            <label class="control_label">Company Name <span class="color-red">*</span></label>
-                        </div>
-                        <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
-                            <div class="input-group">
-                                <div class="input-group-addon"><i class="fa fa-home"></i></div>
-                                <input type="text" class="form-control" id="username" placeholder="Enter Company Name">
-                            </div>
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            <i class="fa fa-phone"></i>  <span>0207 1129 189</span>
                         </div>
                     </div>
                     <div class="row form-group">
-                        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
-                            <label class="control_label">Phone Number <span class="color-red">*</span></label>
-                        </div>
-                        <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
-                            <div class="input-group">
-                                <div class="input-group-addon"><i class="fa fa-phone"></i></div>
-                                <input type="text" class="form-control" id="username" placeholder="Enter Phone Number">
-                            </div>
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            <i class="fa fa-envelope"></i>  <a>info@ukenergybusiness.com</a>
                         </div>
                     </div>
                     <div class="row form-group">
-                        <div class="col-xs-12 col-sm-offset-3 col-sm-9 col-md-offset-3 col-md-9 col-lg-offset-3 col-lg-9">
-                            <div class="check-box">
-                                <label>
-                                    <input type="checkbox"> <span>I am interested in a price comparison for my business energy</span>
-                                </label>
-                            </div>
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            <img src="images/Contact-us.jpg" class="img-responsive" alt="contact us image"/>
                         </div>
-                    </div>
-                    <div class="row form-group">
-                        <div class="col-xs-12 col-sm-offset-3 col-sm-9 col-md-offset-3 col-md-9 col-lg-offset-3 col-lg-9">
-                            <div class="check-box">
-                                <label>
-                                    <input type="checkbox"> <span>I want to reduce my business energy consumption</span>
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row form-group">
-                        <div class="col-xs-12 col-sm-offset-3 col-sm-9 col-md-offset-3 col-md-9 col-lg-offset-3 col-lg-9">
-                            <div class="check-box">
-                                <label>
-                                    <input type="checkbox"> <span>I am interested to join UK Energy Business </span>
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row form-group">
-                        <div class="col-xs-12 col-sm-offset-3 col-sm-9 col-md-offset-3 col-md-9 col-lg-offset-3 col-lg-9">
-                            <div class="check-box">
-                                <label>
-                                    <input type="checkbox"> <span>I need help to understand my bills</span>
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row form-group">
-                        <div class="col-xs-12 col-sm-offset-3 col-sm-9 col-md-offset-3 col-md-9 col-lg-offset-3 col-lg-9">
-                            <textarea class="form-control" rows="5" placeholder="How we can Help?"></textarea>
-                        </div>
-                    </div>
-                    <div class="row form-group">
-                        <div class="col-xs-12 col-sm-offset-3 col-sm-9 col-md-offset-3 col-md-9 col-lg-offset-3 col-lg-9">
-                            <a class="anchor-holder custom-button margin-top-20px">Submit</a>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-offset-1 col-md-5 col-lg-offset-1 col-lg-5 our-office wow fadeInRightBig" data-wow-duration="1s">
-                <div class="row form-group">
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                        <h3>Our Office</h3>
-                    </div>
-                </div>
-                <div class="row form-group">
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                        <i class="fa fa-home"></i>  <span>UK ENERGY BUSINESS </span>
-                    </div>
-                </div>
-                <div class="row form-group">
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                        <i class="fa fa-address-book"></i>  <span>98-102, Cranbrook Road(1st FL) Ilford Essex IG1 4NH</span>
-                    </div>
-                </div>
-                <div class="row form-group">
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                        <i class="fa fa-phone"></i>  <span>0207 1129 189</span>
-                    </div>
-                </div>
-                <div class="row form-group">
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                        <i class="fa fa-envelope"></i>  <a>info@ukenergybusiness.com</a>
-                    </div>
-                </div>
-                <div class="row form-group">
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                        <img src="images/Contact-us.jpg" class="img-responsive" alt="contact us image"/>
                     </div>
                 </div>
             </div>
